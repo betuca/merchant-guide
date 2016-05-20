e92b68f448fde3480b21c393436c65f6e7c38aaa
Betuca <betuca69@gmail.com>
2016-05-19 21:23:04 -0300

    teste commit

--- merchantGuide/src/test/java/exercise/InputFileReaderTest.java
+++ merchantGuide/src/test/java/exercise/InputFileReaderTest.java
@@ -21,6 +21,7 @@ public class InputFileReaderTest {
 	public void shouldReadEmptyFile() throws IOException {
 		List<String> allLines = fileReader.readFromResource(TestConstants.EMPTY_FILE);
 		assertEquals(0, allLines.size());
+		assertEquals(0, allLines.size());
 	}
 
 	@Test
